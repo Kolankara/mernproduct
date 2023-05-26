a50bd02ebc16a1b7a0659816b7a653c06fcbfa86
Kolankara <nandanauly20@gmail.com>
2023-05-26 16:30:59 +0530

    first commit

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
-  return (
+  const navigate = useNavigate();
+  const goToAddpage = () => {
+    navigate("/add");
+  };
+    return (
+
     <div>
         <Container className="d-flex justify-content-center">
         <h1 className="mx-3">Products</h1>
-        <Button variant="success">Add +</Button>
+        <Button variant="success" onClick={goToAddpage}>Add +</Button>
         </Container>
     </div>
   )
